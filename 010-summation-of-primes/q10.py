901b5cf7bc81c39b9889bf97c01bbd0e84508eb3
BlueTot <bluetotkc@gmail.com>
2026-05-18 19:59:51 +0100

    feat(q10): redo q10 in hackerrank

--- 010-summation-of-primes/q10.py
+++ 010-summation-of-primes/q10.py
@@ -1,17 +1,38 @@
+#!/bin/python3
+
+from bisect import bisect_left
+
 n = 2000000
-primes = [True]*n
-primes[0] = False
-primes[1] = False
-list_of_primes = [2]
+is_prime = [True]*n
+is_prime[0] = False
+is_prime[1] = False
+primes = [2]
 
 for num in range(3, n+1, 2):
-    if primes[num]:
-        list_of_primes.append(num)
+    if is_prime[num]:
+        primes.append(num)
         for i in range(num**2, n, num):
-            primes[i] = False
+            is_prime[i] = False
 
-print(len(list_of_primes))
-print(list_of_primes[-1])
-print(sum(list_of_primes))
-# print(list_of_primes[10000])
-# print(list_of_primes)
+# construct prefix sum of prime numbers
+
+total = 0
+prefix = []
+for p in primes:
+    total += p
+    prefix.append(total)
+
+
+def euler10(N: int) -> int:
+
+    # get the largest index for which array[value] <= N
+    # then return its prefix sum
+    
+    index = bisect_left(primes, N+1)
+    return prefix[index-1]
+
+
+n = int(input())
+for _ in range(n):
+    print(euler10(int(input())))
+
