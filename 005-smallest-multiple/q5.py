c486e4f500bd6ab6e88eaf0edd6ee4edc1b74bc7
BlueTot <bluetotkc@gmail.com>
2026-05-18 14:38:24 +0100

    feat(q5): redo q5 in hackerrank

--- 005-smallest-multiple/q5.py
+++ 005-smallest-multiple/q5.py
@@ -1,11 +1,14 @@
-num = 20
-while True:
-    for divisor in range(1, 21):
-        if num % divisor != 0:
-            break
-    else:
-        print(num)
-        break
-    if num % 1000 == 0:
-        print(num)
-    num += 20
+#!/bin/python3
+
+from math import lcm
+
+
+def euler5(N: int) -> int:
+
+    nums = list(range(1, N+1))
+    return lcm(*nums)
+
+
+n = int(input())
+for _ in range(n):
+    print(euler5(int(input())))
