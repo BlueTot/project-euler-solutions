9c2d38b092fe932930de88f6772165d21bac64ce
BlueTot <bluetotkc@gmail.com>
2026-05-18 13:52:36 +0100

    feat(q1): update from hackerrank

--- 001-multiples-of-3-or-5/q1.py
+++ 001-multiples-of-3-or-5/q1.py
@@ -1,1 +1,17 @@
-print(sum([i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]))
+#!/bin/python3
+
+from math import ceil
+
+def sum_to_N(n):
+    return n*(n+1)//2
+
+def euler1(N):
+    multiples_of_3 = 3 * sum_to_N(ceil(N/3 - 1))
+    multiples_of_5 = 5 * sum_to_N(ceil(N/5 - 1))
+    multiples_of_15 = 15 * sum_to_N(ceil(N/15 - 1))
+    return multiples_of_3 + multiples_of_5 - multiples_of_15
+
+n = int(input())
+for _ in range(n):
+    print(euler1(int(input())))
+
