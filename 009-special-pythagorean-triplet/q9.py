f93d996e452c6c0c7458cae212b6eb992f694e08
BlueTot <bluetotkc@gmail.com>
2026-05-18 19:49:50 +0100

    feat(q9): redo q9 in hackerrank

--- 009-special-pythagorean-triplet/q9.py
+++ 009-special-pythagorean-triplet/q9.py
@@ -1,6 +1,32 @@
-for a in range(3, 999):
-    for b in range(3, 999):
-        c = (a**2 + b**2)**0.5
-        if int(c) == c and a+b+c == 1000:
-            print(a*b*c)
+#!/bin/python3
+
+
+# we are given a+b+c = N, so c = N-b-a
+# we must have c**2 = a**2 + b**2
+# substituting c and rearranging, we get
+# b = (N**2 - 2Na)/(2N-2a)
+
+# provides us with a linear time algorithm
+# by iterating through all values of a
+
+def euler9(N: int) -> int:
+
+    res = -1
+
+    for a in range(3, N):
+        q, r = N*N - 2*N*a, 2*N - 2*a
+        if (q % r != 0):
+            continue
+        b = q // r
+        if (b > 0 and N-b-a > 0):
+            abc = a * b * (N-b-a)
+            res = max(res, abc)
+
+    return res
+
+
+n = int(input())
+for _ in range(n):
+    print(euler9(int(input())))
+
 
