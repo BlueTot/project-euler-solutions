af1cc14e82a0710c552c2453beb93beeef3ff0e7
BlueTot <bluetotkc@gmail.com>
2026-05-29 16:57:38 +0100

    feat(q18): redo q18 in hackerrank

--- 018-maximum-path-sum-i/q18.py
+++ 018-maximum-path-sum-i/q18.py
@@ -1,8 +1,19 @@
-tri = [[int(x) for x in row.split(' ')] for row in open("triangle.txt").readlines()]
-print(tri)
+#!/bin/python3
 
-maxRow = len(tri)-2
-for row in range(maxRow, -1, -1):
-    for col in range(row+1):
-        tri[row][col] += max(tri[row+1][col], tri[row+1][col+1])    
-print(tri[0][0])
+
+def euler18(tri: list[list[int]]) -> int:
+    max_row = len(tri)-2
+    for row in range(max_row, -1, -1):
+        for col in range(row+1):
+            tri[row][col] += max(tri[row+1][col], tri[row+1][col+1])    
+    return tri[0][0]
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    tri = []
+    for _ in range(n):
+        tri.append(list(map(int, input().split())))
+    print(euler18(tri))
+
