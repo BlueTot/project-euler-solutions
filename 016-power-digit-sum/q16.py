cce971d3c652b70e152dae2ba89b96744ef46907
BlueTot <bluetotkc@gmail.com>
2026-05-29 16:45:52 +0100

    feat(q16): redo q16 in hackerrank

--- 016-power-digit-sum/q16.py
+++ 016-power-digit-sum/q16.py
@@ -1,1 +1,9 @@
-print(sum([int(char) for char in str(2**1000)]))
+#!/bin/python3
+
+def euler16(N: int) -> int:
+    return sum([int(char) for char in str(1 << N)])
+
+t = int(input())
+for _ in range(t):
+    print(euler16(int(input())))
+
